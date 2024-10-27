0c847c0b6cc794e6975b4b77daee3eca42d195c7
JaagupPr <158137390+JaagupPr@users.noreply.github.com>
2024-10-28 00:17:32 +0200

    SpaceshipDetails

--- ShopTARge23/ShopTARge23/Controllers/SpaceshipsController.cs
+++ ShopTARge23/ShopTARge23/Controllers/SpaceshipsController.cs
@@ -1,12 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge23.Core.ServiceInterface;
+using ShopTARge23.Data;
+using ShopTARge23.Models.Spaceships;
 
 namespace ShopTARge23.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopTARge23Context _context;
+        private readonly ISpaceshipsServices _spaceshipServices;
+
+        public SpaceshipsController
+        (        
+                        ShopTARge23Context context,
+                        ISpaceshipsServices spaceshipsServices
+        )
+        {
+            _context = context;
+            _spaceshipServices = spaceshipsServices;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Spaceships
+            .Select(x => new SpaceshipsIndexViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Typename = x.Typename,
+                BuiltDate = x.BuiltDate,
+                Crew = x.Crew,
+            });
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var spaceship = await _spaceshipServices.DetailAsync(id);
+
+            if (spaceship == null)
+            {
+                return View("Error");
+            }
+
+            var vm = new SpaceshipDetailsViewModel();
+
+            vm.Id = spaceship.Id;
+            vm.Name = spaceship.Name;
+            vm.Typename = spaceship.Typename;
+            vm.BuiltDate = spaceship.BuiltDate;
+            vm.SpaceshipModel = spaceship.SpaceshipModel;
+            vm.Crew = spaceship.Crew;
+            vm.EnginePower = spaceship.EnginePower;
+            vm.CreatedAt = spaceship.CreatedAt;
+            vm.ModifiedAt = spaceship.ModifiedAt;
+
+
+            return View(vm);
         }
     }
 }
