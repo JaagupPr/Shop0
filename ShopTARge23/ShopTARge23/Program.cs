fa0b85944a1693a343a231427eebb643569ebf5f
JaagupPr <158137390+JaagupPr@users.noreply.github.com>
2024-11-19 17:11:41 +0200

    ApiCallFreeToGames

--- ShopTARge23/ShopTARge23/Program.cs
+++ ShopTARge23/ShopTARge23/Program.cs
@@ -20,6 +20,7 @@ namespace ShopTARge23
             builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
             builder.Services.AddScoped<IChuckNorrisServices, ChuckNorrisServices>();
             builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+            builder.Services.AddScoped<IFreeToGamesServices, FreeToGamesServices>();
             builder.Services.AddDbContext<ShopTARge23Context>(options =>
                            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
